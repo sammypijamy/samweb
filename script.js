f5645fd0836a2eb8320ffba2a2ab6d02bfee34e7
sammypijamy <samaragransky@outlook.com>
2022-07-25 16:35:26 -0700

    add text, img1 and style css

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript, I kinda hate js lol";
+
+h2.textContent = "Welcome!";
 
 document.querySelector("body").appendChild(h2);
 
